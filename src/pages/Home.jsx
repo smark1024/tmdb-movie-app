8508640ef273e3fb1d8b16c3bb0b9c2ad7cd5fdd
smark1024 <kyehyun1024@gmail.com>
2025-12-10 20:17:51 +0900

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <h1>홈 페이지</h1>
+      <p>영화 목록이 여기에 표시될 예정입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
+
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/_variables.scss" as *; @use "@/styles/_mixins.scss" as *;`,
+        api: 'modern-compiler'
+      }
+    }
+  }
+})
+
